9f27a8ab205d6086f85144a5f01c5dee6c75cd7a
Developer-Aryan <aryansharma189911@gmail.com>
2023-06-21 00:23:11 +0530

    First Draft of the website

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  // Add your component code here
+
+  return (
+    <div>
+      <h1>Welcome to Your Website</h1>
+    </div>
+  );
+}
+
+export default App;
